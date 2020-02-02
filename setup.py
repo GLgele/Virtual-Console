f363fedfd82ba3f58a4104b29d42c3bd379a5953
GLgele <ai_minecraft@163.com + GLgele@outlook.com>
2020-02-02 13:12:48 +0800

    Update

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #-*- coding:utf-8 -*-
 
-#############################################
+####################################################
 # File Name: setup.py
-# Author: mage
-# Mail: 
+# Author: GLgele
+# Mail: ai_minecraft@163.com + GLgele@outlook.com
 # Created Time:  2020/2/2 13:03:38
-#############################################
+####################################################
 
 
 from setuptools import setup, find_packages
@@ -13,14 +13,14 @@ from setuptools import setup, find_packages
 setup(
     name = "VC_cn",
     version = "1.0.0",
-    keywords = ("pip","),
-    description = "time and path tool",
+    keywords = ("pip","VC_cn"),
+    description = "Virtual Console",
     long_description = "time and path tool",
-    license = "MIT Licence",
+    license = "None",
 
-    url = "https://github.com/fengmm521/pipProject",
-    author = "mage",
-    author_email = "mage@woodcol.com",
+    url = "https://github.com/GLgele/Virtual-Console-cn",
+    author = "GLgele",
+    author_email = "ai_minecraft@163.com",
 
     packages = find_packages(),
     include_package_data = True,
